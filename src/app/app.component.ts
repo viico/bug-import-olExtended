5b8caa0b95b70664611964019084e35e5d6fff03
Victor LOIZEL <victor.loizel.ext@groupama.com>
2023-10-25 14:40:33 +0200

    commit avec le bug

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {View, Map } from 'ol';
+import {olExtended} from 'geoportal-extensions-openlayers';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'bug-import-olExtended';
+
+  ngOnInit() {
+    var map = new Map({
+      target : "map",
+      layers : [
+        /*new olExtended.layer.GeoportalWMTS({
+          layer : "GEOGRAPHICALGRIDSYSTEMS.MAPS"
+        })*/
+      ],
+      view : new View({
+        center : [288074.8449901076, 6247982.515792289],
+        zoom : 8
+      })
+    });
+  }
 }
